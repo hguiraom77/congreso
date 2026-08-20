4302f775c3c9d62376cce8fea238f1aab985e67e
hguiraom77 <clase.hjgm.itss@gmail.com>
2026-08-20 13:31:38 +0000

    CREATED estructura base

---
+++ src/routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventoController;
+use App\Http\Controllers\PonenteController;
+use App\Http\Controllers\AsistenteController;
+
+// Rutas de eventos
+Route::get('/eventos', [EventoController::class, 'index']);
+Route::post('/eventos', [EventoController::class, 'store']);
+Route::get('/eventos/{id}', [EventoController::class, 'show']);
+Route::put('/eventos/{id}', [EventoController::class, 'update']);
+Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
+
+// Rutas de ponentes
+Route::apiResource('ponentes', PonenteController::class);
+
+// Rutas de asistentes
+Route::apiResource('asistentes', AsistenteController::class);
